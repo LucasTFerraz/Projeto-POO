fabfc3e63d325b058f37bf67f6f1ad2699afdfc3
LucasTFerraz <lucastferraz@hotmail.com>
2026-08-31 17:25:18 -0300

    Delete codigo-aulas directory
    deleting unnecessary files and folders 3

--- codigo-aulas/2026-07-06/06-lacos-robo.py
+++
@@ -1,5 +0,0 @@
-n = int(input("Quantos comandos para o robô? "))
-for i in range(n):
-    comando = input(f'Digite o comando #{i+1}: ')
-    print(f'Executando {comando}')
-print('Chegamos ao fim.')
--- codigo-aulas/2026-07-06/07-lacos-robo.py
+++
@@ -1,7 +0,0 @@
-COMANDO_SAIDA = "SAIR"
-while True:
-    comando = input(f'Digite o comando: ')
-    if comando == COMANDO_SAIDA:
-        break
-    print(f'Executando {comando}')
-print("Programa encerrado.")
